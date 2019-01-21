c8d5c85b6db7304230434e3800eb5d8ed9999a5d
antoniomangiacapra <antonio.mangiacapra@drivek.com>
2019-01-21 20:53:34 +0100

    Create Math class and display the result in a wp_die()

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -5,6 +5,13 @@ namespace AntonioEatGoat\UnittestLesson;
 class Bootstrap {
 
 	public static function init() {
+		$math = new Math( 5, 2 );
+
+		try {
+			wp_die( $math->divide() );
+		} catch ( \RuntimeException $e ) {
+			wp_die( $e->getMessage() );
+		}
 	}
 
 }
---
+++ src/Math.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AntonioEatGoat\UnittestLesson;
+
+
+class Math {
+
+	/**
+	 * @var int
+	 */
+	private $operand1;
+	/**
+	 * @var int
+	 */
+	private $operand2;
+
+	/**
+	 * Math constructor.
+	 *
+	 * @param int $operand1
+	 * @param int $operand2
+	 */
+	public function __construct( int $operand1, int $operand2 ) {
+		$this->operand1 = $operand1;
+		$this->operand2 = $operand2;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function sum(): int {
+		return $this->operand1 + $this->operand2;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function subtract(): int {
+		return $this->operand1 - $this->operand2;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function multiply(): int {
+		return $this->operand1 * $this->operand2;
+	}
+
+	/**
+	 * @return float
+	 *
+	 * @throws \RuntimeException
+	 */
+	public function divide(): float {
+		if( 0 === $this->operand2 ) {
+			throw new \RuntimeException( 'Wrong operation: you canont divide by 0.' );
+		}
+
+		return round( $this->operand1 / $this->operand2, 2 );
+	}
+}
